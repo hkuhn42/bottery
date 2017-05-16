0d8af2efaef1e65cf1942274df2529dbf8509b47
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2017-05-16 22:46:02 +0200

    Added propertyName, renamed name

--- bottery.core/src/main/java/rocks/bottery/bot/util/SessionPropertyModel.java
+++ bottery.core/src/main/java/rocks/bottery/bot/util/SessionPropertyModel.java
@@ -1,17 +1,14 @@
 /**
- *    Copyright (C) 2016-2017 Harald Kuhn
+ * Copyright (C) 2016-2017 Harald Kuhn
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 /**
  * 
@@ -32,17 +29,19 @@ public class SessionPropertyModel<T extends Serializable> implements ISessionMod
 
 	private static final long serialVersionUID = 1L;
 
-	private String			  name;
+	private String			  beanName;
+	private String			  propertyName;
 
-	public SessionPropertyModel(String name) {
-		this.name = name;
+	public SessionPropertyModel(String beanName, String propertyName) {
+		this.beanName = beanName;
+		this.propertyName = propertyName;
 	}
 
 	@Override
 	public void setObject(T object, ISession session) {
-		Object target = session.getAttribute(name);
+		Object target = session.getAttribute(beanName);
 		try {
-			PropertyUtils.setProperty(target, name, object);
+			PropertyUtils.setProperty(target, propertyName, object);
 		}
 		catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
@@ -60,9 +59,9 @@ public class SessionPropertyModel<T extends Serializable> implements ISessionMod
 
 	@Override
 	public T getObject(ISession session) {
-		Object target = session.getAttribute(name);
+		Object target = session.getAttribute(beanName);
 		try {
-			return (T) PropertyUtils.getProperty(target, name);
+			return (T) PropertyUtils.getProperty(target, beanName);
 		}
 		catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
