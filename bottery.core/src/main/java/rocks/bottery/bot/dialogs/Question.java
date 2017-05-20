c10ef2da5c76b14dbc71dc4649ea05eebbccbf70
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2017-05-20 18:03:21 +0200

    added validation support, added intent attribiutes support
    - answers to questions can be validated with a validator
    - question optionally checks the intents attributes for an answer, too

--- bottery.core/src/main/java/rocks/bottery/bot/dialogs/Question.java
+++ bottery.core/src/main/java/rocks/bottery/bot/dialogs/Question.java
@@ -1,17 +1,14 @@
 /**
- *    Copyright (C) 2016-2017 Harald Kuhn
+ * Copyright (C) 2016-2017 Harald Kuhn
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 /**
  * 
@@ -41,6 +38,10 @@ public abstract class Question<T> extends DialogBase {
 	private ISessionModel<T> answerModel;
 
 	private IModel<String>	 confirmText;
+	/**
+	 * a key for looking up an answer from an intents attributes
+	 */
+	private String			 answerIntentKey;
 
 	public Question(ISessionModel<T> anwerModel) {
 		this.answerModel = anwerModel;
@@ -60,28 +61,70 @@ public abstract class Question<T> extends DialogBase {
 	@Override
 	public void handle(ISession session, IActivity activity) {
 		T answer = null;
+		// check wether we have asked for this session
 		Object alreadyAsked = session.getAttribute(String.valueOf(this.hashCode()));
 		logger.debug("asked code =  " + alreadyAsked);
 		if (alreadyAsked != null) {
 			String text = activity.getText();
-			answer = findAnswer(text);
+			try {
+				answer = findAnswer(text, activity, session);
+			}
+			catch (InvalidAnswerException e) {
+				session.send(e.getFeedback());
+			}
+
+			if (answer != null) {
+				answered(answer, activity, session);
+				return;
+			}
 		}
-		if (answer != null) {
-			answered(answer, session, activity);
-			return;
+		else {
+			// if not call super (utterance will output the question)
+			super.handle(session, activity);
+			// and remember in the stream
+			session.setAttribute(String.valueOf(this.hashCode()), "asked");
 		}
-		session.setAttribute(String.valueOf(this.hashCode()), "asked");
-		super.handle(session, activity);
 	}
 
-	protected void answered(T answer, ISession session, IActivity answerActivity) {
+	/**
+	 * Callback if the response to the question is detected
+	 * 
+	 * @param answer
+	 * @param answerActivity
+	 * @param session
+	 */
+	protected void answered(T answer, IActivity answerActivity, ISession session) {
 		session.removeAttribut("asked");
 		answerModel.setObject(answer, session);
 		session.activeDialogFinished(answerActivity);
 	}
 
-	protected T findAnswer(String text) {
-		return convert(text);
+	/**
+	 * Tries to find the answer either in the response text or in the detected intends attributes
+	 * 
+	 * @param answerText
+	 * @param answerActivity
+	 * @param session
+	 * @return
+	 * @throws InvalidAnswerException
+	 */
+	protected T findAnswer(String answerText, IActivity answerActivity, ISession session) throws InvalidAnswerException {
+		T answer = null;
+		if (answerActivity.getIntent() != null && answerActivity.getIntent().getAttribute(answerIntentKey) != null) {
+			answer = convert(String.valueOf(answerActivity.getIntent().getAttribute(answerIntentKey)));
+		}
+		else {
+			answer = convert(answerText);
+		}
+		if (answerValidator != null) {
+			ValidationResult result = answerValidator.validate(answer, answerText, session);
+			if (!result.isValid()) {
+				IActivity feedback = session.getConnector().newReplyTo(answerActivity);
+
+				throw new InvalidAnswerException(feedback);
+			}
+		}
+		return answer;
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -104,6 +147,14 @@ public abstract class Question<T> extends DialogBase {
 
 	public void setConfirmText(IModel<String> confirmText) {
 		this.confirmText = confirmText;
+	}
+
+	public String getAnswerIntentKey() {
+		return answerIntentKey;
+	}
+
+	public void setAnswerIntentKey(String answerIntentKey) {
+		this.answerIntentKey = answerIntentKey;
 	}
 
 }
