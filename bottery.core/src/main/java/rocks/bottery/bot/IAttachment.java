f43a3fb6e01ba42d048a91f59c594587bca119d2
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2017-06-04 09:24:31 +0200

    cleanup and doc
    - added javadoc
    - removed typing methods from session (should a seperate activity)
    - removed unused messageController
    - bumped version to 0.51
    - added ignores for eclipse project files

--- bottery.core/src/main/java/rocks/bottery/bot/IAttachment.java
+++ bottery.core/src/main/java/rocks/bottery/bot/IAttachment.java
@@ -1,17 +1,14 @@
 /**
- *    Copyright (C) 2016-2017 Harald Kuhn
+ * Copyright (C) 2016-2017 Harald Kuhn
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package rocks.bottery.bot;
 
@@ -19,18 +16,44 @@ import java.io.Serializable;
 import java.net.URL;
 
 /**
+ * An attachment to an activity depending on the connector different types of attachments may be supported
+ * 
  * @author Harald Kuhn
- *
  */
 public interface IAttachment extends Serializable {
 
+	/**
+	 * the type of attachment
+	 * 
+	 * @return
+	 */
 	public AttachmentType getType();
 
+	/**
+	 * the size of the attachment
+	 * 
+	 * @return
+	 */
 	public long getSize();
 
+	/**
+	 * the name
+	 * 
+	 * @return
+	 */
 	public String getName();
 
+	/**
+	 * a unique id for the attachment
+	 * 
+	 * @return
+	 */
 	public String getId();
 
+	/**
+	 * the url to lookup the attachment
+	 * 
+	 * @return
+	 */
 	public URL getUrl();
 }
