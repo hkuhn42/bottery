e060df9ab7bedbd31ed4c9f52c10528ce69e7289
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2017-10-29 22:14:58 +0100

    Added map for additional fields

--- bottery.connector.ms/src/main/java/rocks/bottery/bot/connector/ms/model/Entity.java
+++ bottery.connector.ms/src/main/java/rocks/bottery/bot/connector/ms/model/Entity.java
@@ -1,19 +1,19 @@
 /**
- *    Copyright (C) 2016-2017 Harald Kuhn
+ * Copyright (C) 2016-2017 Harald Kuhn
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package rocks.bottery.bot.connector.ms.model;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -26,48 +26,61 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import org.codehaus.jackson.annotate.JsonAnyGetter;
+import org.codehaus.jackson.annotate.JsonAnySetter;
+
 @XmlAccessorType(XmlAccessType.FIELD)
- @XmlType(name = "Entity", propOrder =
-	{ "type"
-})
+@XmlType(name = "Entity", propOrder = { "type" })
 
-@XmlRootElement(name="Entity")
-public class Entity  {
-  
+@XmlRootElement(name = "Entity")
+public class Entity {
 
-  @XmlElement(name="type")
-  private String type = null;
+	@XmlElement(name = "type")
+	private String				  type	= null;
 
-  /**
-   * Entity Type (typically from schema.org types)
-   **/
-  
-  public String getType() {
-    return type;
-  }
-  public void setType(String type) {
-    this.type = type;
-  }
+	// and then "other" stuff:
+	protected Map<String, Object> other	= new HashMap<String, Object>();
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Entity {\n");
-    
-    sb.append("    type: ").append(toIndentedString(type)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+	/**
+	 * Entity Type (typically from schema.org types)
+	 **/
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private static String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class Entity {\n");
+
+		sb.append("    type: ").append(toIndentedString(type)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces (except the first line).
+	 */
+	private static String toIndentedString(Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
+
+	// "any getter" needed for serialization
+	@JsonAnyGetter
+	public Map<String, Object> any() {
+		return other;
+	}
+
+	@JsonAnySetter
+	public void set(String name, Object value) {
+		other.put(name, value);
+	}
 }
-
