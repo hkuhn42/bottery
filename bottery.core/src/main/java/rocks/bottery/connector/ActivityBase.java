3416e94ccc8225c6f55f05feb77e9fb4db5bc967
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2018-04-08 20:23:03 +0200

    Common base class for Activities

---
+++ bottery.core/src/main/java/rocks/bottery/connector/ActivityBase.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2016-2018 Harald Kuhn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package rocks.bottery.connector;
+
+import java.io.Serializable;
+
+import rocks.bottery.bot.IActivity;
+import rocks.bottery.bot.recognizers.IIntent;
+
+/**
+ * Abstract base class for activities
+ * 
+ * @author Harald Kuhn
+ */
+public abstract class ActivityBase implements IActivity, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private IIntent			  intent;
+
+	public ActivityBase() {
+		super();
+	}
+
+	@Override
+	public IIntent getIntent() {
+		return intent;
+	}
+
+	@Override
+	public void setIntent(IIntent intent) {
+		this.intent = intent;
+	}
+
+}
