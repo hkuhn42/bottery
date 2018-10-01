2c6d4dabcab334272a8d893ae4730038fb70c7c4
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2018-10-01 23:16:31 +0200

    Cleanup

--- bottery.core/src/main/java/rocks/bottery/messaging/MessagingContext.java
+++ bottery.core/src/main/java/rocks/bottery/messaging/MessagingContext.java
@@ -10,9 +10,6 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-/**
- * 
- */
 package rocks.bottery.messaging;
 
 import rocks.bottery.bot.ContextBase;
