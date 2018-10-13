66357d0bf39d15f8194d840d4bf1074e4e4b7e3e
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2018-10-13 09:37:29 +0200

    Renamed TelegramConnector to TelegramBotConnector
    In order to later support the telegram client api
    the connector for the Bot api was renamed

--- bottery.examples/src/main/java/rocks/bottery/examples/hello/telegram/HelloTelegram.java
+++ bottery.examples/src/main/java/rocks/bottery/examples/hello/telegram/HelloTelegram.java
@@ -1,9 +1,21 @@
+/**
+ * Copyright (C) 2016-2018 Harald Kuhn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package rocks.bottery.examples.hello.telegram;
 
 import org.apache.log4j.BasicConfigurator;
 
-import rocks.bottery.connector.TelegramConnector;
-import rocks.bottery.examples.EchoBot;;
+import rocks.bottery.connector.TelegramBotConnector;
+import rocks.bottery.examples.AttaBot;;
 
 /**
  * This example uses the msConnector for a hello world
@@ -12,6 +24,6 @@ public class HelloTelegram {
 
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
-		new TelegramConnector("telegram").register(new EchoBot());
+		new TelegramBotConnector("telegram").register(new AttaBot());
 	}
 }
