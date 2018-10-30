ac87e7773b946b2ba78ba3271bced313db5e180a
harald.kuhn@gmail.com <harald.kuhn@gmail.com>
2018-10-30 20:54:04 +0100

    added map to handle additional fields

--- bottery.connector.ms/src/main/java/rocks/bottery/connector/ms/model/ConversationAccount.java
+++ bottery.connector.ms/src/main/java/rocks/bottery/connector/ms/model/ConversationAccount.java
@@ -12,6 +12,9 @@
  */
 package rocks.bottery.connector.ms.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
@@ -23,76 +26,94 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import org.codehaus.jackson.annotate.JsonAnyGetter;
+import org.codehaus.jackson.annotate.JsonAnySetter;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 @XmlAccessorType(XmlAccessType.FIELD)
- @XmlType(name = "ConversationAccount", propOrder =
-	{ "isGroup", "id", "name"
-})
+@XmlType(name = "ConversationAccount", propOrder = { "isGroup", "id", "name" })
 
-@XmlRootElement(name="ConversationAccount")
-public class ConversationAccount  {
-  
+@XmlRootElement(name = "ConversationAccount")
+public class ConversationAccount {
 
-  @XmlElement(name="isGroup")
-  private Boolean isGroup = null;
+	@XmlElement(name = "isGroup")
+	private Boolean				isGroup				 = null;
 
-  @XmlElement(name="id")
-  private String id = null;
+	@XmlElement(name = "id")
+	private String				id					 = null;
 
-  @XmlElement(name="name")
-  private String name = null;
+	@XmlElement(name = "name")
+	private String				name				 = null;
 
-  /**
-   * Is this a reference to a group
-   **/
-  
-  public Boolean getIsGroup() {
-    return isGroup;
-  }
-  public void setIsGroup(Boolean isGroup) {
-    this.isGroup = isGroup;
-  }
-  /**
-   * Channel id for the user or bot on this channel (Example: joe@smith.com, or @joesmith or 123456)
-   **/
-  
-  public String getId() {
-    return id;
-  }
-  public void setId(String id) {
-    this.id = id;
-  }
-  /**
-   * Display friendly name
-   **/
-  
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
+	@JsonIgnore
+	private Map<String, Object>	additionalProperties = new HashMap<String, Object>();
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class ConversationAccount {\n");
-    
-    sb.append("    isGroup: ").append(toIndentedString(isGroup)).append("\n");
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    name: ").append(toIndentedString(name)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+	/**
+	 * Is this a reference to a group
+	 **/
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private static String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	public Boolean getIsGroup() {
+		return isGroup;
+	}
+
+	public void setIsGroup(Boolean isGroup) {
+		this.isGroup = isGroup;
+	}
+
+	/**
+	 * Channel id for the user or bot on this channel (Example: joe@smith.com, or @joesmith or 123456)
+	 **/
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * Display friendly name
+	 **/
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class ConversationAccount {\n");
+
+		sb.append("    isGroup: ").append(toIndentedString(isGroup)).append("\n");
+		sb.append("    id: ").append(toIndentedString(id)).append("\n");
+		sb.append("    name: ").append(toIndentedString(name)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private static String toIndentedString(Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-
